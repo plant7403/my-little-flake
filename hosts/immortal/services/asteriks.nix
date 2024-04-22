e6b5548959eb36d0285e5945e9ad3b6a986796f2
Egor <egor@luna>
2024-04-22 21:45:27 +0700

    add ppp

--- hosts/immortal/services/asteriks.nix
+++ hosts/immortal/services/asteriks.nix
@@ -92,4 +92,8 @@
   # we had a sepearte VLAN for this, so *shrug*
   # makes things easier if I don't have to keep track of ports
   #networking.firewall.enable = false;
+
+  environment.systemPackages = with pkgs; [
+    ppp
+  ];
 }
