9adc1b1fe07b4947375ed92c82ed6bda6b6c6b32
me <me@o.o>
2025-12-18 10:44:03 +0100

    changes from stellar on jue 18 dic 2025 10:44:02 CET

--- home-manager/desktop/vscode.nix
+++ home-manager/desktop/vscode.nix
@@ -358,7 +358,7 @@ in
                    };
           */
             "material-icon-theme.folders.color": "#8695b7";
-  "material-icon-theme.folders.theme": "specific";
+  "material-icon-theme.folders.theme"= "specific";
   "material-icon-theme.hidesExplorerArrows" =  true;
         }
         // baseSettings;
