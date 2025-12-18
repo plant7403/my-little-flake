30b6f98d630ea350a185ebc421520569e71e8591
me <me@o.o>
2025-12-18 09:21:48 +0100

    changes from stellar on jue 18 dic 2025 09:21:48 CET

--- home-manager/desktop/extensions.nix
+++ home-manager/desktop/extensions.nix
@@ -183,6 +183,11 @@
          workspace-names = [ "Main" ];
        };
     */
+    "com/github/amezin/ddterm/panel-icon-type" = "none";
+    "com/github/amezin/ddterm/background-opacity" = 0.5;
+    "com/github/amezin/ddterm/hide-when-focus-lost" = true;
+    "com/github/amezin/ddterm/hide-window-on-esc" = true;
+    "com/github/amezin/ddterm/tab-label-ellipsize-mode" = "start";
   };
 
   /*
