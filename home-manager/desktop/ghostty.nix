3b33abb0f14325d01aa8d1d6d0ea74b0570c0ee4
me <me@o.o>
2025-12-18 10:55:35 +0100

    changes from stellar on jue 18 dic 2025 10:55:35 CET

--- home-manager/desktop/ghostty.nix
+++ home-manager/desktop/ghostty.nix
@@ -11,8 +11,8 @@
         "ctrl+h=goto_split:left"
         "ctrl+l=goto_split:right"
       ];
-      quit-after-last-window-closed = true
-quit-after-last-window-closed-delay = 5m
+      quit-after-last-window-closed = true;
+      quit-after-last-window-closed-delay = 5 m;
     };
     systemd.enable = true;
   };
