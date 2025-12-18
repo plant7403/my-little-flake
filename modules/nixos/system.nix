2fe3735060787b6dad14074f04e75a1436dbaf70
me <me@o.o>
2025-12-18 03:23:35 +0100

    changes from stellar on jue 18 dic 2025 03:23:35 CET

--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -68,8 +68,8 @@ in
           "https://nix-community.cachix.org"
         ];
         trusted-public-keys = [
-          "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
-          "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+          #"cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
+          #"devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         ];
