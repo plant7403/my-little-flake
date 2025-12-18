b1ec117f9ad7fe84be3cff82639dbd20ae753800
me <me@o.o>
2025-12-18 04:19:46 +0100

    changes from stellar on jue 18 dic 2025 04:19:46 CET

--- home-manager/core/zsh.nix
+++ home-manager/core/zsh.nix
@@ -127,13 +127,11 @@
         ];
       };
 
-      completionInit = ''
+      /*
+        completionInit = ''
 
-      '';
-
-      initContent = ''
-        
-      '';
+           '';
+      */
 
     };
   };
