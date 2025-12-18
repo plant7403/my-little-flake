da4b6cba9c7698713e98fe38f808c21d16164f20
me <me@o.o>
2025-12-18 05:42:51 +0100

    changes from stellar on jue 18 dic 2025 05:42:51 CET

--- home-manager/core/zsh.nix
+++ home-manager/core/zsh.nix
@@ -196,7 +196,7 @@ in
   programs.starship.settings = {
     "$schema" = "https://starship.rs/config-schema.json";
 
-    format = ''
+    format = lib.concatStrings ''
       [](#9A348E)\
       $os\
       $username\
