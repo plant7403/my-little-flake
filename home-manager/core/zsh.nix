efc702972e087af684d07ac43479c46b7856db4c
me <me@o.o>
2025-12-18 04:47:42 +0100

    changes from stellar on jue 18 dic 2025 04:47:42 CET

--- home-manager/core/zsh.nix
+++ home-manager/core/zsh.nix
@@ -129,7 +129,7 @@
           { name = "autojump"; }
           { name = "git"; }
           { name = "colored-man-pages"; }
-          
+          {name = }
           { name = "zsh-users/zsh-completions"; }
           { name = "zsh-users/zsh-autosuggestions"; }
           { name = "zsh-users/history"; }
