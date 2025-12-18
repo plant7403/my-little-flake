daf816af3674c5db93185b11aed8af07d075dead
me <me@o.o>
2025-12-18 04:35:05 +0100

    changes from stellar on jue 18 dic 2025 04:35:05 CET

--- home-manager/core/zsh.nix
+++ home-manager/core/zsh.nix
@@ -64,7 +64,7 @@
         (lib.mkBefore ''
           #POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true
           #source ~/.p10k.zsh
-            # Powerlevel10k Zsh theme  
+          # Powerlevel10k Zsh theme  
           source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme  
           test -f ~/.config/zsh/.p10k.zsh && source ~/.config/zsh/.p10k.zsh 
 
