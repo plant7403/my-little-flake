e6e41b8e56080674a3f8788543d311a938ea8150
me <me@o.o>
2025-12-18 09:37:56 +0100

    changes from stellar on jue 18 dic 2025 09:37:56 CET

--- home-manager/desktop/extensions.nix
+++ home-manager/desktop/extensions.nix
@@ -186,7 +186,7 @@
     "com/github/amezin/ddterm" = {
       panel-icon-type = "none";
       background-opacity = "0.5";
-      hide-when-focus-lost = true
+      hide-when-focus-lost = true;
       hide-window-on-esc = true;
       tab-label-ellipsize-mode = "start";
     };
