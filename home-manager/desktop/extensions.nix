cd8fff874c84599a751ac93dfb57e9c9406d7178
me <me@o.o>
2025-12-18 09:12:43 +0100

    changes from stellar on jue 18 dic 2025 09:12:43 CET

--- home-manager/desktop/extensions.nix
+++ home-manager/desktop/extensions.nix
@@ -74,11 +74,11 @@
       ];
     };
     "org/gnome/settings-daemon/plugins/media-keys" = {
-      "/custom-keybindings" = {
-        "/custom0" = {
-          "/binding" = [ "<Shift><Control>w" ];
-          "/command" = "ulauncher";
-          "/name" = "ulauncher";
+      "custom-keybindings" = {
+        "custom0" = {
+          "binding" = [ "<Shift><Control>w" ];
+          "command" = "ulauncher";
+          "name" = "ulauncher";
         };
       };
       "www" = [ "<Shift><Control>s" ];
