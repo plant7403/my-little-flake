1dcb310893e8838570b5146ace2a611439ad206d
me <me@o.o>
2025-12-18 03:32:24 +0100

    changes from stellar on jue 18 dic 2025 03:32:23 CET

--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -101,6 +101,12 @@ in
           "vscode-extension-github-copilot-chat"
           "obsidian"
         ];
+      programs.nh = {
+        enable = true;
+        clean.enable = true;
+        clean.extraArgs = "--keep-since 4d --keep 3";
+        flake = "/home/user/my-nixos-config"; # sets NH_OS_FLAKE variable for you
+      };
       #nix.settings.download-buffer-size = 524288000;
 
       networking.hostName = cfg.hostname; # Define your hostname.
