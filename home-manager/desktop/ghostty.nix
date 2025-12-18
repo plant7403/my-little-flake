078831ab4d80be229b0551ffb672ab0fc3950e59
me <me@o.o>
2025-12-18 10:55:31 +0100

    changes from stellar on jue 18 dic 2025 10:55:30 CET

--- home-manager/desktop/ghostty.nix
+++ home-manager/desktop/ghostty.nix
@@ -11,6 +11,8 @@
         "ctrl+h=goto_split:left"
         "ctrl+l=goto_split:right"
       ];
+      quit-after-last-window-closed = true
+quit-after-last-window-closed-delay = 5m
     };
     systemd.enable = true;
   };
