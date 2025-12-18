52318056f731c08218d7092a22a4094d146b5f1f
me <me@o.o>
2025-12-18 06:25:06 +0100

    changes from stellar on jue 18 dic 2025 06:25:06 CET

--- home-manager/core/zsh.nix
+++ home-manager/core/zsh.nix
@@ -161,10 +161,7 @@ in
               "depth:1"
             ];
           }
-          {
-            name = "zsh-users/zsh-history-substring-search";
-            tags = [ "defer:3" ];
-          }
+          
           {
             name = "axieax/zsh-starship";
             /*
