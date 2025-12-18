773daeb612a6d69463bf6ce30f41612fb1406381
me <me@o.o>
2025-12-18 09:09:32 +0100

    changes from stellar on jue 18 dic 2025 09:09:32 CET

--- hosts/common/optional/nix-flatpak.nix
+++ hosts/common/optional/nix-flatpak.nix
@@ -6,7 +6,7 @@
 
   services.flatpak.enable = true;
   #services.flatpak.remotes = [{ name = "flathub-beta"; location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo"; }];
-  services.flatpak.update.onActivation = true;
+  #services.flatpak.update.onActivation = true;
   /*
     services.flatpak.update = {
       auto = {
