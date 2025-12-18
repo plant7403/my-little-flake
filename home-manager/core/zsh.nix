dfcf8691b88cc49ecace03abfb43ee4eb4d9c52b
me <me@o.o>
2025-12-18 05:18:12 +0100

    changes from stellar on jue 18 dic 2025 05:18:12 CET

--- home-manager/core/zsh.nix
+++ home-manager/core/zsh.nix
@@ -121,13 +121,13 @@ in
         enable = true;
         plugins = [
           { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
-          {
-            name = "romkatv/powerlevel10k";
-            tags = [
-              "as:theme"
-              "depth:1"
-            ];
-          } # Installations with additional options. For the list of options, please refer to Zplug README.
+          #{
+          #  name = "romkatv/powerlevel10k";
+          #  tags = [
+          #    "as:theme"
+          #    "depth:1"
+          #  ];
+          #} # Installations with additional options. For the list of options, please refer to Zplug README.
           #{ name = "powerlevel10k-config"; }
           { name = "autojump"; }
           { name = "git"; }
