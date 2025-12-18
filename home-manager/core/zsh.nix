98cb00ce3c6bc21385943b7eed2027f87754f559
me <me@o.o>
2025-12-18 04:54:36 +0100

    changes from stellar on jue 18 dic 2025 04:54:36 CET

--- home-manager/core/zsh.nix
+++ home-manager/core/zsh.nix
@@ -171,10 +171,23 @@
   */
   #programs.starship.configPath
 
-programs.starship.enableInteractive = true;
+  programs.starship.enableInteractive = true;
 
-programs.starship.enableTransience = true;
-programs.starship.enable = true;
-programs.starship.enableZshIntegration = true;
-programs.starship.settings
+  programs.starship.enableTransience = true;
+  programs.starship.enable = true;
+  programs.starship.enableZshIntegration = true;
+  #programs.starship.settings = {
+  add_newline = false;
+  format = lib.concatStrings [
+    "$line_break"
+    "$package"
+    "$line_break"
+    "$character"
+  ];
+  scan_timeout = 10;
+  character = {
+    success_symbol = "➜";
+    error_symbol = "➜";
+  };
+};
 }
