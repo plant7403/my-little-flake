a1d2d4f8331348a479200b7863ac5aec075659f3
me <me@o.o>
2025-12-18 08:52:56 +0100

    changes from stellar on jue 18 dic 2025 08:52:55 CET

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -150,7 +150,7 @@
         entry = true;
         image = "debian:13";
         init_hooks = [
-          "ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/docker"
+          "ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/podman"
           "ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/docker-compose"
         ];
       };
