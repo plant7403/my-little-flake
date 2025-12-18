31efac1c94ee3a3c810c8e509470ef6b914f0392
me <me@o.o>
2025-12-18 05:49:44 +0100

    changes from stellar on jue 18 dic 2025 05:49:44 CET

--- home-manager/desktop/vscode.nix
+++ home-manager/desktop/vscode.nix
@@ -243,25 +243,7 @@ in
         userSettings = {
           # Theme Setup.
           "workbench.colorTheme" = "Shades of Purple";
-          "workbench.iconTheme" = "vscode-icons";
-          "editor.fontFamily" = "Operator Mono, Menlo, Monaco, 'Courier New', monospace";
-          "terminal.integrated.fontFamily" = "'Operator Mono', 'Inconsolata for Powerline', monospace";
-          "editor.fontSize" = 17;
-          "editor.lineHeight" = 24.65;
-          "editor.letterSpacing" = 0.5;
-          "editor.fontWeight" = "400";
-          "editor.fontLigatures" = true;
-          "editor.cursorStyle" = "line";
-          "editor.cursorWidth" = 5;
-          "editor.cursorBlinking" = "solid";
-          "editor.renderWhitespace" = "all";
-          "editor.snippetSuggestions" = "top";
-          "workbench.startupEditor" = "newUntitledFile";
-          "editor.glyphMargin" = true;
-          "workbench.editor.enablePreview" = false;
-          "explorer.confirmDragAndDrop" = false;
-          "files.trimTrailingWhitespace" = true;
-          "files.trimFinalNewlines" = true;
+          
           # Formatting Optional.
           "editor.formatOnSave" = true;
           "prettier.eslintIntegration" = true;
