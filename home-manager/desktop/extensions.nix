01bf9390fdff9101c1d3fd45d60848b34f902478
me <me@o.o>
2025-12-18 09:04:16 +0100

    changes from stellar on jue 18 dic 2025 09:04:16 CET

--- home-manager/desktop/extensions.nix
+++ home-manager/desktop/extensions.nix
@@ -75,9 +75,9 @@
     };
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings" custom0/binding
   '<Shift><Control>w'
-custom0/command
+command
   'ulauncher'
-custom0/name
+name
   'ulauncher'
 
     "org/gnome/mutter" = {
