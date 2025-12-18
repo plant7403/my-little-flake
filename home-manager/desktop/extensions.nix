8029cca6c765f730a628b19d0163aaa93429a643
me <me@o.o>
2025-12-18 09:06:28 +0100

    changes from stellar on jue 18 dic 2025 09:06:28 CET

--- home-manager/desktop/extensions.nix
+++ home-manager/desktop/extensions.nix
@@ -79,6 +79,9 @@
         "command" = "ulauncher";
         "name" = "ulauncher";
       };
+      /org/gnome/settings-daemon/plugins/media-keys/www
+  ['<Shift><Control>s']
+
     };
     "org/gnome/mutter" = {
       "edge-tiling" = true;
