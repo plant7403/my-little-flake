2fea982118dc0cc85c6b8f9c5739c822931b038f
me <me@o.o>
2025-12-18 09:07:03 +0100

    changes from stellar on jue 18 dic 2025 09:07:03 CET

--- home-manager/desktop/extensions.nix
+++ home-manager/desktop/extensions.nix
@@ -79,7 +79,7 @@
         "command" = "ulauncher";
         "name" = "ulauncher";
       };
-      "www" = ['<Shift><Control>s']
+      "www" = ['<Shift><Control>s"];
 
     };
     "org/gnome/mutter" = {
