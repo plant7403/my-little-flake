2f7bd1e92b1b40af695594ae452f4ddc3851a3d5
me <me@o.o>
2025-12-18 10:40:18 +0100

    changes from stellar on jue 18 dic 2025 10:40:18 CET

--- home-manager/core/zsh.nix
+++ home-manager/core/zsh.nix
@@ -91,6 +91,15 @@
         "pkill *"
         "cp *"
       ];
+      # With Oh-My-Zsh:
+      oh-my-zsh = {
+        enable = true;
+        plugins = [
+          "git" # also requires `programs.git.enable = true;`
+          "thefuck" # also requires `programs.thefuck.enable = true;`
+        ];
+        theme = "robbyrussell";
+      };
       plugins = [
         {
           name = "zsh-autosuggestions";
