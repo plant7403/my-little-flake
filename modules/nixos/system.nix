8c95d23e6cd7adb416f557f6c0b4cde1353d2232
me <me@o.o>
2025-12-19 18:07:48 +0100

    changes from stellar on vie 19 dic 2025 18:07:48 CET

--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -155,15 +155,7 @@ in
         nixos-rebuild-ng
 
       ];
-      # programs.command-not-found.enable = false;
-      # for home-manager, use programs.bash.initExtra instead
-      /*
-        programs.zsh.interactiveShellInit = ''
-             source ${pkgs.nix-index}/etc/profile.d/command-not-found.sh
-           '';
-      */
-      #programs.zsh.ohMyZsh.enable = true;
-      #environment.pathsToLink = [ "/share/zsh" ]; # ???
+
       documentation = {
         nixos.enable = true;
         man = {
