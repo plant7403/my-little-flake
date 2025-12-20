416d6b6a2e7421707e69cb53aca630e656bcce8a
me <me@o.o>
2025-12-20 10:52:44 +0100

    changes from stellar on sáb 20 dic 2025 10:52:44 CET

--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -190,26 +190,7 @@ in
               xonsh.xontribs.xontrib-whole-word-jumping
               xonsh.xontribs.xontrib-direnv
             */
-            (buildPythonPackage {
-              name = "xontrib-gitinfo";
-              pyproject = true;
-              build-system = [
-                setuptools
-              ];
-              src = pkgs.fetchFromGitHub {
-                owner = "dyuri";
-                repo = "xontrib-gitinfo";
-                rev = "b1ba458d85a6684088807d962b39980144685630";
-                sha256 = "sha256-e5lgfcrG8p/3YgYNlnkfZIYj3VEjuNTRoseAl+Uyfd8=";
-              };
 
-              meta = {
-                homepage = "https://github.com/dyuri/xontrib-gitinfo";
-                description = "Plugin";
-                license = pkgs.lib.licenses.mit;
-                maintainers = [ ];
-              };
-            })
           ];
       };
       programs.zoxide.enableXonshIntegration = true;
