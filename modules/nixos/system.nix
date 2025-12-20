bc9beb75b04e32fe30f7eca5ab75d81cbf889f30
me <me@o.o>
2025-12-20 10:53:47 +0100

    changes from stellar on sáb 20 dic 2025 10:53:46 CET

--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -191,7 +191,7 @@ in
               xonsh.xontribs.xontrib-direnv
             */
           ];
-          package = pkgs.xonsh.override
+          package = pkgs.xonsh.override {extra}
       };
       programs.zoxide.enableXonshIntegration = true;
       programs.direnv.enableXonshIntegration = true;
