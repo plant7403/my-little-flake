494da894c7e64fba0ca7c488014c25baf74bc51a
me <me@o.o>
2025-12-20 11:11:12 +0100

    changes from stellar on sáb 20 dic 2025 11:11:12 CET

--- hosts/common/users/egor.nix
+++ hosts/common/users/egor.nix
@@ -12,7 +12,7 @@ in
   users.mutableUsers = false;
   users.users.egor = {
     isNormalUser = true;
-    shell = pkgs.zsh;
+    shell = pkgs.xosh;
     extraGroups = [
       "audio"
       "networkmanager"
