dec9136f63de0c3b682acb230bbfc89374389f83
me <me@o.o>
2025-12-21 08:53:28 +0100

    changes from stellar on dom 21 dic 2025 08:53:27 CET

--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -175,7 +175,7 @@ in
         bashCompletion.enable = true;
         extraPackages = ps: [
           #setuptools
-          numpy
+          ps.numpy
           xonsh.xontribs.xontrib-vox
           #xonsh.xontribs.gitinfo
           #xonsh.xontribs.prompt_starship
