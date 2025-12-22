f5851b88d1d1428aae07b0b65361eb6b65f69301
me <me@o.o>
2025-12-22 21:14:23 +0100

    changes from stellar on lun 22 dic 2025 21:14:23 CET

--- home-manager/desktop/extensions.nix
+++ home-manager/desktop/extensions.nix
@@ -74,7 +74,7 @@
       ];
     };
     "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings" =
-  ["/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/']
+  ["/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0"/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/']
 
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
       "binding" = [ "<Shift><Control>u" ];
