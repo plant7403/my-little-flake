a514927abd9fc043bc653014960db8b44c14be1a
me <me@o.o>
2025-12-22 20:48:05 +0100

    changes from stellar on lun 22 dic 2025 20:48:04 CET

--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -67,18 +67,18 @@ in
           always-allow-substitutes = true;
           # Use the binary cache aggressively
           substituters = [
-            "<https://cache.nixos.org"
-            "<https://nix-community.cachix.org>"
+            "https://cache.nixos.org"
+            "https://nix-community.cachix.org>"
             "<https://nixpkgs-wayland.cachix.org>"
           ];
-          /*
+
             trusted-public-keys = [
               #"cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
               #"devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
               "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
               "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
             ];
-          */
+
 
           trusted-users = [
             "root"
