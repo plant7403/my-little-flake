9490973745f456cbdbdf0107ea905657a428bd70
me <me@o.o>
2025-12-22 22:52:07 +0100

    changes from stellar on lun 22 dic 2025 22:52:07 CET

--- tools/fs-diff.sh
+++ tools/fs-diff.sh
@@ -37,15 +37,9 @@ cut -f2- -d'/' |
 sort |
 uniq |
 while read path; do
-  #if [ $1 = -r ]; then
-  #  path="/$path"
-  #elif [ $1 = -h ]
-  #  path="$path"
   if [ "$1" = -r ]; then
     path="/$path"
-
   fi
-
 
   if [ -L "$path" ]; then
     : # The path is a symbolic link, so is probably handled by NixOS already
