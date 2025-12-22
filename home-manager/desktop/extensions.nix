af8ded5515af90dd580bf2c796cbbb19e5f1ddad
me <me@o.o>
2025-12-22 21:47:06 +0100

    changes from stellar on lun 22 dic 2025 21:47:05 CET

--- home-manager/desktop/extensions.nix
+++ home-manager/desktop/extensions.nix
@@ -78,6 +78,7 @@
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
       ];
+      search = [ "<super>space" ];
     };
 
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
