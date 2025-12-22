3b9d7c73f3c42004ef3cc19d1cf8f674a8dadc3c
me <me@o.o>
2025-12-22 19:00:05 +0100

    changes from stellar on lun 22 dic 2025 19:00:04 CET

--- home-manager/desktop/vscode.nix
+++ home-manager/desktop/vscode.nix
@@ -78,9 +78,9 @@ let
     #"vscode-pets.theme" = "forest";
     #"vscode-pets.throwBallWithMouse" = false;
 
-nixEnvSelector.nixFile	null	Path to the Nix config file
-nixEnvSelector.packages	[]	List packages using as -p nix-shell args
-nixEnvSelector.args	null	Custom args string for nix-shell. EX: -A <something> --pure
+nixEnvSelector.nixFile	null	#Path to the Nix config file
+nixEnvSelector.packages	[]	#List packages using as -p nix-shell args
+nixEnvSelector.args	null	#Custom args string for nix-shell. EX: -A <something> --pure
 nixEnvSelector.nixShellPath	null	Custom path for nix-shell executable
 nixEnvSelector.useFlakes	false
   };
