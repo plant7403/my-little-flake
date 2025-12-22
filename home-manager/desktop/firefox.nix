399db8a4c4c79dcf58287902b6d2dcac37aa3ca0
me <me@o.o>
2025-12-22 23:37:15 +0100

    changes from stellar on lun 22 dic 2025 23:37:15 CET

--- home-manager/desktop/firefox.nix
+++ home-manager/desktop/firefox.nix
@@ -104,7 +104,7 @@
             name = "Nix Packages";
             urls = [
               {
-                template = "https://search.nixos.org/packages";
+                template = "https://search.nixos.org/packages?channel=unstable&query=authelia";
                 params = [
                   {
                     name = "type";
