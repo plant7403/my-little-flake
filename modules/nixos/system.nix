8712e16dc4e67da9fe76e36dc9ba44ea11cdd0b7
me <me@o.o>
2025-12-22 19:56:49 +0100

    changes from stellar on lun 22 dic 2025 19:56:48 CET

--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -71,12 +71,14 @@ in
             "<https://nix-community.cachix.org>"
             "<https://nixpkgs-wayland.cachix.org>"
           ];
-          trusted-public-keys = [
-            #"cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
-            #"devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-          ];
+          /*
+            trusted-public-keys = [
+              #"cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
+              #"devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+              "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+              "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+            ];
+          */
 
           trusted-users = [
             "root"
