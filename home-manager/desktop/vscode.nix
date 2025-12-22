3ac6e6f7e484eea37ebfa9d6440fe31f4513adbb
me <me@o.o>
2025-12-22 12:28:31 +0100

    changes from stellar on lun 22 dic 2025 12:28:31 CET

--- home-manager/desktop/vscode.nix
+++ home-manager/desktop/vscode.nix
@@ -347,6 +347,7 @@ in
 
             "danielpinto8zz6.c-cpp-project-generator"
             "franneck94.vscode-c-cpp-config"
+            "harry-ross-software.c-snippets"
           ]
           ++ forOpenVsx baseExtensions
           ++ forVscode [
