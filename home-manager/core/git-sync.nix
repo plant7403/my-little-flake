b16a665dd162eff3c641e27993b0009adc2780d4
me <me@o.o>
2025-12-23 18:28:17 +0100

    changes from stellar on mar 23 dic 2025 18:28:16 CET

--- home-manager/core/git-sync.nix
+++ home-manager/core/git-sync.nix
@@ -5,7 +5,7 @@
     repositories = {
       my-little-flake = {
         extraPackages = with pkgs; [ git-crypt ];
-        interval = 1;
+        interval = 2800;
         path = "/home/egor/my-little-flake";
         uri = "git+ssh://git@git.disroot.org/me/my-little-flake.git";
       };
