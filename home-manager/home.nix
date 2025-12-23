469a39cc12bcfcaa6efe9a567fdd60cf74ce6c8f
me <me@o.o>
2025-12-23 12:31:31 +0100

    changes from stellar on mar 23 dic 2025 12:31:30 CET

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -202,7 +202,7 @@
       identity_url = "https://passwords.pak.academy/identity";
       base_url = "https://passwords.pak.academy/";
       ui_url = "https://passwords.pak.academy/";
-      notifications_url = "https://passwords.pak.academy/"
+      notifications_url = "https://passwords.pak.academy/notifications"
       pinentry = pkgs.pinentry-curses;
     };
   };
