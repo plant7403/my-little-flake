70914b8c9f22986bd170c0ca9021ec705e85741d
me <me@o.o>
2025-12-23 03:21:09 +0100

    changes from stellar on mar 23 dic 2025 03:21:09 CET

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -181,8 +181,8 @@
 
   programs.rbw.settings.email = "@getgoogleoff.me";
   programs.rbw.settings.identity_url =
-  programs.rbw.settings.base_url =
-  programs.rbw.enable = turel
+  programs.rbw.settings.base_url = 
+  programs.rbw.enable = true;
 
   programs.distrobox = {
     containers = {
