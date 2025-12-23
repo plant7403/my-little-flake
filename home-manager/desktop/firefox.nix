48c5197139c71514e893dddae7f99a6f0b789697
me <me@o.o>
2025-12-23 08:29:28 +0100

    changes from stellar on mar 23 dic 2025 08:29:28 CET

--- home-manager/desktop/firefox.nix
+++ home-manager/desktop/firefox.nix
@@ -143,7 +143,7 @@
         # Enable userContent.css and userChrome.css for our theme modules
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
         # Stop creating ~/Downloads!
-        "browser.download.dir" = "${config.user.home}/Downloads";
+        "browser.download.dir" = "${config.egor.home}/Downloads";
         # Don't use the built-in password manager. A nixos user is more likely
         # using an external one (you are using one, right?).
         "signon.rememberSignons" = false;
