ab475d6ba773730d9d9b18d1758b3440ce30031d
me <me@o.o>
2025-12-23 08:45:03 +0100

    changes from stellar on mar 23 dic 2025 08:45:03 CET

--- home-manager/desktop/ffpwa.nix
+++ home-manager/desktop/ffpwa.nix
@@ -1,7 +1,7 @@
 {
 
-programs.firefoxpwa.enable"";
-programs.firefoxpwa.package"";
+programs.firefoxpwa.enable="";
+programs.firefoxpwa.package="";
 programs.firefoxpwa.settings= "";
 programs.firefoxpwa.profiles = {
 "Default".name = "";
