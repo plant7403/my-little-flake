755f2727a6a9e4686f77b5a1b80dba105ab688b5
me <me@o.o>
2025-12-23 08:21:48 +0100

    changes from stellar on mar 23 dic 2025 08:21:48 CET

--- home-manager/desktop/firefox.nix
+++ home-manager/desktop/firefox.nix
@@ -312,7 +312,7 @@
 
 
 font.size.variable.x-western" = 20
-browser.toolbars.bookmarks.visibility","always"
+browser.toolbars.bookmarks.visibility" ="always";
 privacy.resisttFingerprinting.letterboxing", true);
 network.http.referer.XOriginPolicy",2);
 privacy.clearOnShutdown.history",true);
