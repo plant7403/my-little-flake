8e423fe772dd15292665cd555563b4e74659ec74
me <me@o.o>
2025-12-23 08:44:42 +0100

    changes from stellar on mar 23 dic 2025 08:44:42 CET

--- home-manager/desktop/ffpwa.nix
+++ home-manager/desktop/ffpwa.nix
@@ -4,16 +4,13 @@ programs.firefoxpwa.enable
 programs.firefoxpwa.package
 programs.firefoxpwa.settings
 programs.firefoxpwa.profiles = {
-Default.name = "";
-Default.settings= "";
-Default.sites."Cloudflare".desktopEntry.categories= "";
-Default.sites."Cloudflare".desktopEntry.enable= "";
-Default.sites."Cloudflare".desktopEntry.icon= "";
-Default.sites."Cloudflare".manifestUrl= "";
-Default.sites."Cloudflare".name= "";
-Default.sites."Cloudflare".settings= "";
-Default.sites."Cloudflare".url= "";
-
-
-
+"Default".name = "";
+"Default".settings= "";
+"Default".sites."Cloudflare".desktopEntry.categories= "";
+"Default".sites."Cloudflare".desktopEntry.enable= "";
+"Default".sites."Cloudflare".desktopEntry.icon= "";
+"Default".sites."Cloudflare".manifestUrl= "";
+"Default".sites."Cloudflare".name= "";
+"Default".sites."Cloudflare".settings= "";
+"Default".sites."Cloudflare".url= "";
 }
