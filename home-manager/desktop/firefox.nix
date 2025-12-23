6dcab4635a518372fbe0ffac7324e091e909f601
me <me@o.o>
2025-12-23 09:24:36 +0100

    changes from stellar on mar 23 dic 2025 09:24:35 CET

--- home-manager/desktop/firefox.nix
+++ home-manager/desktop/firefox.nix
@@ -73,7 +73,7 @@
         "privacy.clearOnShutdown_v2.cache" = 0;
         "privacy.clearOnShutdown_v2.cookiesAndStorage" = 0;
         #"extensions.activeThemeID" = "coffee_theme_firefox";
-        #"browser.theme.toolbar-theme" = 0;
+        #!"browser.theme.toolbar-theme" = 0;
         "privacy.resistFingerprinting.letterboxing" = false;
         "middlemouse.paste" = false;
         "browser.uiCustomization.state" = builtins.toJSON {
