aa1e68b981e8c9104f520f5353d68fe7f2c6cac4
me <me@o.o>
2025-12-23 08:21:18 +0100

    changes from stellar on mar 23 dic 2025 08:21:18 CET

--- home-manager/desktop/firefox.nix
+++ home-manager/desktop/firefox.nix
@@ -306,3 +306,27 @@
     };
   };
 }
+
+
+
+
+
+      defaultPref("font.size.variable.x-western",20);
+      defaultPref("browser.toolbars.bookmarks.visibility","always");
+      defaultPref("privacy.resisttFingerprinting.letterboxing", true);
+      defaultPref("network.http.referer.XOriginPolicy",2);
+      defaultPref("privacy.clearOnShutdown.history",true);
+      defaultPref("privacy.clearOnShutdown.downloads",true);
+      defaultPref("privacy.clearOnShutdown.cookies",true);
+      defaultPref("gfx.webrender.software.opengl",false);
+      defaultPref("webgl.disabled",true);
+
+      pref("font.size.variable.x-western",20);
+      pref("browser.toolbars.bookmarks.visibility","always");
+      pref("privacy.resisttFingerprinting.letterboxing", true);
+      pref("network.http.referer.XOriginPolicy",2);
+      pref("privacy.clearOnShutdown.history",true);
+      pref("privacy.clearOnShutdown.downloads",true);
+      pref("privacy.clearOnShutdown.cookies",true);
+      pref("gfx.webrender.software.opengl",false);
+      pref("webgl.disabled",true);
