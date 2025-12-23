92f91b926ad9bb25a3fb3fff2f0011dc4e249bbd
me <me@o.o>
2025-12-23 08:48:07 +0100

    changes from stellar on mar 23 dic 2025 08:48:07 CET

--- home-manager/desktop/ffpwa.nix
+++ home-manager/desktop/ffpwa.nix
@@ -8,13 +8,13 @@
       settings = "";
       sites = {
         "Cloudflare" = {
-    name = "MDN Web Docs";
-    url = "https://developer.mozilla.org/";
-    manifestUrl = "https://developer.mozilla.org/manifest.f42880861b394dd4dc9b.json";
-    desktopEntry.icon = pkgs.fetchurl {
-      url = "https://developer.mozilla.org/favicon-192x192.png";
-      sha256 = "0p8zgf2ba48l2pq1gjcffwzmd9kfmj9qc0v7zpwf2qd54fndifxr";
-    };
+          name = "MDN Web Docs";
+          url = "https://developer.mozilla.org/";
+          manifestUrl = "https://developer.mozilla.org/manifest.f42880861b394dd4dc9b.json";
+          desktopEntry.icon = pkgs.fetchurl {
+            url = "https://developer.mozilla.org/favicon-192x192.png";
+            sha256 = "0p8zgf2ba48l2pq1gjcffwzmd9kfmj9qc0v7zpwf2qd54fndifxr";
+          };
       };
     };
   };
