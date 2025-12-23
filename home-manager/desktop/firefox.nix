804a4537cb31a5fa70b99f0254fcd5a9e10bf0b4
me <me@o.o>
2025-12-23 07:21:44 +0100

    changes from stellar on mar 23 dic 2025 07:21:44 CET

--- home-manager/desktop/firefox.nix
+++ home-manager/desktop/firefox.nix
@@ -30,18 +30,22 @@
         "extensions.update.enabled" = false;
       };
       userChrome = ''
-  /* Hide tab bar in FF Quantum */
-  @-moz-document url(chrome://browser/content/browser.xul), url(chrome://browser/content/browser.xhtml) {
-    #TabsToolbar {
-      visibility: collapse !important;
-      margin-bottom: 21px !important;
-    }
+        /* Hide tab bar in FF Quantum */
+        @-moz-document url(chrome://browser/content/browser.xul), url(chrome://browser/content/browser.xhtml) {
+          #TabsToolbar {
+            visibility: collapse !important;
+            margin-bottom: 21px !important;
+          }
 
-    #sidebar-box[sidebarcommand="treestyletab_piro_sakura_ne_jp-sidebar-action"] #sidebar-header {
-      visibility: collapse !important;
-    }
-  }
-''
+          #sidebar-box[sidebarcommand="treestyletab_piro_sakura_ne_jp-sidebar-action"] #sidebar-header {
+            visibility: collapse !important;
+          }
+        }
+      '';
+      userContent = ''
+        /* Hide scrollbar in FF Quantum */
+        *{scrollbar-width:none !important}
+      '';
       containersForce = true;
       extensions = {
         force = true;
