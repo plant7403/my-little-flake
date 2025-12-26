bfd6a5c376fc6cc9774a5f2edf593df7ccc03780
me <me@o.o>
2025-12-26 11:36:29 +0100

    changes from stellar on vie 26 dic 2025 11:36:28 CET

--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -121,7 +121,7 @@ in
           keep-going = true
           log-lines = 20
 
-          experimental-features = nix-command flakes
+
           #!include ${config.sops.secrets."system/nix-token".path}
         '';
       };
