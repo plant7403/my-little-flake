32a97f4839443d9f401b372087f0a97f1f9776f3
me <me@o.o>
2025-12-27 11:51:23 +0100

    changes from stellar on sáb 27 dic 2025 11:51:22 CET

--- home-manager/desktop/firefox.nix
+++ home-manager/desktop/firefox.nix
@@ -75,7 +75,7 @@
         "browser.theme.toolbar-theme" = 0;
         #"privacy.resistFingerprinting.letterboxing" = false;
         "middlemouse.paste" = false;
-        
+
         # Default to dark theme in DevTools panel
         "devtools.theme" = "dark";
         # Set browser to dark theme
@@ -340,6 +340,51 @@
         # Extensions
         "browser.policies.runOncePerModification.extensionsInstall" =
           "[\"https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi\", \"https://addons.mozilla.org/firefox/downloads/latest/cookie-autodelete/latest.xpi\", \"https://addons.mozilla.org/firefox/downloads/latest/decentraleyes/latest.xpi\", \"https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi\", \"https://addons.mozilla.org/firefox/downloads/latest/uaswitcher/latest.xpi\"]";
+        "browser.uiCustomization.state" = builtins.toJSON {
+          currentVersion = 20;
+          newElementCount = 23;
+          dirtyAreaCache = [
+            "nav-bar"
+            "unified-extensions-area"
+            "PersonalToolbar"
+            "TabsToolbar"
+            "widget-overflow-fixed-list"
+            "toolbar-menubar"
+          ];
+          seen = [
+            "developer-button"
+            "_0050e3fa-15cc-4fb6-9c73-7354489a810b_-browser-action"
+            "ublock0_raymondhill_net-browser-action"
+            "_a138007c-5ff6-4d10-83d9-0afaf0efbe5e_-browser-action"
+          ];
+          placements = {
+            nav-bar = [
+              "back-button"
+              "forward-button"
+              "stop-reload-button"
+              "urlbar-container"
+              "unified-extensions-button"
+              "fxa-toolbar-menu-button"
+            ];
+            PersonalToolbar = [ "personal-bookmarks" ];
+            TabsToolbar = [
+              "tabbrowser-tabs"
+              "alltabs-button"
+            ];
+            toolbar-menubar = [ "menubar-items" ];
+            unified-extensions-area = [
+              "ublock0_raymondhill_net-browser-action"
+              "_0050e3fa-15cc-4fb6-9c73-7354489a810b_-browser-action"
+              "_a138007c-5ff6-4d10-83d9-0afaf0efbe5e_-browser-action"
+            ];
+            widget-overflow-fixed-list = [
+              "downloads-button"
+              "developer-button"
+              "firefox-view-button"
+              "characterencoding-button"
+            ];
+          };
+        };
       };
       /*
         userChrome = ''
