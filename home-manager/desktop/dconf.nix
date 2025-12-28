bec717807553a9c38c9fa8f387787af1764564f6
me <me@o.o>
2025-12-29 00:13:24 +0100

    changes from stellar on lun 29 dic 2025 00:13:24 CET

--- home-manager/desktop/dconf.nix
+++ home-manager/desktop/dconf.nix
@@ -8,7 +8,7 @@ with lib.hm.gvariant;
     "com/github/amezin/ddterm" = {
       background-opacity = 0.5;
       bold-is-bright = true;
-      hide-animation-duration = ;
+      hide-animation-duration = 0ю5;
       hide-when-focus-lost = true;
       hide-window-on-esc = true;
       panel-icon-type = "none";
