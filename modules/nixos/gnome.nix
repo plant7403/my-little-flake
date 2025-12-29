30b45c5e48aee971cfa774a7ca46c67f2b730805
me <me@o.o>
2025-12-29 22:56:45 +0100

    changes from stellar on lun 29 dic 2025 22:56:45 CET

--- modules/nixos/gnome.nix
+++ modules/nixos/gnome.nix
@@ -72,6 +72,19 @@ in
       xdg.portal.enable = true;
       services.gvfs.enable = true;
 
+      nixpkgs.overlays = [
+        (final: prev: {
+          nautilus = prev.nautilus.overrideAttrs (nprev: {
+            buildInputs =
+              nprev.buildInputs
+              ++ (with pkgs.gst_all_1; [
+                gst-plugins-good
+                gst-plugins-bad
+              ]);
+          });
+        })
+      ];
+
       environment.systemPackages = with pkgs; [
         gnome-tweaks
         libsecret
