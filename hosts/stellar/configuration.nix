945618e49780f1db0d1159ca52aea82494a83e21
me <me@o.o>
2025-12-29 19:35:38 +0100

    changes from stellar on lun 29 dic 2025 19:35:38 CET

--- hosts/stellar/configuration.nix
+++ hosts/stellar/configuration.nix
@@ -286,7 +286,26 @@
     }
   '';
   services.dashy.virtualHost.domain = "localhost";
-
-  
   #services.dashy.finalDrv
+
+  services.nginx = {
+    enable = true;
+    virtualHosts."blog.example.com" = {
+      enableACME = true;
+      forceSSL = true;
+      root = "/var/www/blog";
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [
+    
+  ];
+
+  security.acme = {
+    # Accept the CA’s terms of service. The default provider is Let’s Encrypt, you can find their ToS at https://letsencrypt.org/repository/.
+    acceptTerms = true;
+    # Optional: You can configure the email address used with Let's Encrypt.
+    # This way you get renewal reminders (automated by NixOS) as well as expiration emails.
+    defaults.email = "youremail@address.com";
+  };
 }
