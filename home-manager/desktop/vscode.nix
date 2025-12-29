b055c766ee17635472bdb87925d8c3588caa6e7b
me <me@o.o>
2025-12-29 22:39:33 +0100

    changes from stellar on lun 29 dic 2025 22:39:32 CET

--- home-manager/desktop/vscode.nix
+++ home-manager/desktop/vscode.nix
@@ -36,11 +36,11 @@ let
     "editor.guides.bracketPairs" = true;
     "editor.wordWrap" = "off";
     "diffEditor.wordWrap" = "off";
-    "explorer.compactFolders": true,
-    "explorer.decorations.badges": true,
-    "explorer.fileNesting.enabled": false,
-    "explorer.openEditors.sortOrder": "fullPath",
-    "explorer.sortOrder": "type",
+    "explorer.compactFolders" = true,
+    "explorer.decorations.badges" = true,
+    "explorer.fileNesting.enabled" = false,
+    "explorer.openEditors.sortOrder" = "fullPath",
+    "explorer.sortOrder" = "type",
     # "blockman.n33A01B2FromDepth0ToInwardForAllBackgrounds" = "10,0,0,1; none";
     #"editor.fontFamily" = "Operator Mono, Menlo, Monaco, 'Courier New', monospace";
     #"terminal.integrated.fontFamily" = "'Operator Mono', 'Inconsolata for Powerline', monospace";
