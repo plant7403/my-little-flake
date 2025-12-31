d8da612a91bdf3c8be5669ff17d24217dddc6a28
me <me@o.o>
2025-12-31 11:33:34 +0100

    changes from stellar on mié 31 dic 2025 11:33:33 CET

--- flake.nix
+++ flake.nix
@@ -475,7 +475,7 @@
       };
 
       # This is highly advised, and will prevent many possible mistakes
-      checks = builtins.mapAttrs (_system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
+      #checks = builtins.mapAttrs (_system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
 
     };
 }
