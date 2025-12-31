85dd16616899d987b9a3515190708ebcdbc575a4
me <me@o.o>
2025-12-31 05:57:01 +0100

    changes from stellar on mié 31 dic 2025 05:57:01 CET

--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -503,84 +503,88 @@ in
         enableDebugInfo = true;
         enableNotifications = true;
 
-        freeMemKillThreshold = "";
+        #freeMemKillThreshold = "";
         freeMemThreshold = "4";
         freeSwapKillThreshold = "";
         freeSwapThreshold = "2";
         reportInterval = "";
-         extraArgs = let
-      # applications that we would like to avoid killing
-      # when system is under high memory pressure
-      appsToAvoid = concatStringsSep "|" [
-        "Hyprland" # avoid killing the graphical session
-        "foot" # terminal, might have unsaved files
-        "cryptsetup" # avoid killing the disk encryption manager
-        "dbus-daemon" # avoid killing the dbus daemon
-        "dbus-broker" # on newer, nixos versions broker is the default
-        "Xwayland" # avoid killing the X11 server
-        "gpg-agent" # avoid killing the gpg agent
-      ];
+        extraArgs =
+          let
+            # applications that we would like to avoid killing
+            # when system is under high memory pressure
+            appsToAvoid = concatStringsSep "|" [
+              "Hyprland" # avoid killing the graphical session
+              "foot" # terminal, might have unsaved files
+              "cryptsetup" # avoid killing the disk encryption manager
+              "dbus-daemon" # avoid killing the dbus daemon
+              "dbus-broker" # on newer, nixos versions broker is the default
+              "Xwayland" # avoid killing the X11 server
+              "gpg-agent" # avoid killing the gpg agent
+            ];
 
-      # apps that we would like killed first
-      # those are likely the ones draining most memory
-      appsToPrefer = concatStringsSep "|" [
-        # browsers
-        "Web Content"
-        "Isolated Web Co"
-        "chromium"
-        # electron applications
-        "electron" # I wish we could kill electron permanently
-        ".*.exe"
-        "java"
-        # added 2024-05-12: PipeWire locked down my system as it failed to acquire RT privileges
-        "pipewire(.*)" # catch pipewire and pipewire-pulse
-      ];
-    in [
-      "-g" # kill all processes within a process group
-      "--avoid '^(${appsToAvoid})$'" # things we want to not kill
-      "--prefer '^(${appsToPrefer})$'" # things we want to kill as soon as possible
-    ];
+            # apps that we would like killed first
+            # those are likely the ones draining most memory
+            appsToPrefer = concatStringsSep "|" [
+              # browsers
+              "Web Content"
+              "Isolated Web Co"
+              "chromium"
+              # electron applications
+              "electron" # I wish we could kill electron permanently
+              ".*.exe"
+              "java"
+              # added 2024-05-12: PipeWire locked down my system as it failed to acquire RT privileges
+              "pipewire(.*)" # catch pipewire and pipewire-pulse
+            ];
+          in
+          [
+            "-g" # kill all processes within a process group
+            "--avoid '^(${appsToAvoid})$'" # things we want to not kill
+            "--prefer '^(${appsToPrefer})$'" # things we want to kill as soon as possible
+          ];
 
-    # we should ideally write the logs into a designated log file; or even better, to the journal
-    # for now we can hope this echo sends the log to somewhere we can observe later
-    killHook = pkgs.writeShellScript "earlyoom-kill-hook" ''
-      echo "Process $EARLYOOM_NAME ($EARLYOOM_PID) was killed"
-    '';
-  };
+        # we should ideally write the logs into a designated log file; or even better, to the journal
+        # for now we can hope this echo sends the log to somewhere we can observe later
+        killHook = pkgs.writeShellScript "earlyoom-kill-hook" ''
+          echo "Process $EARLYOOM_NAME ($EARLYOOM_PID) was killed"
+        '';
+      };
 
-    };
-  systemd.services.earlyoom.serviceConfig = {
-    # from upstream
-    DynamicUser = true;
-    AmbientCapabilities = "CAP_KILL CAP_IPC_LOCK";
-    Nice = -20;
-    OOMScoreAdjust = -100;
-    ProtectSystem = "strict";
-    ProtectHome = true;
-    Restart = "always";
-    TasksMax = 10;
-    MemoryMax = "50M";
+      systemd.services.earlyoom.serviceConfig = {
+        # from upstream
+        DynamicUser = true;
+        AmbientCapabilities = "CAP_KILL CAP_IPC_LOCK";
+        Nice = -20;
+        OOMScoreAdjust = -100;
+        ProtectSystem = "strict";
+        ProtectHome = true;
+        Restart = "always";
+        TasksMax = 10;
+        MemoryMax = "50M";
 
-    # rotection rules. Mostly from the `systemd-oomd` service
-    # with some of them already included upstream.
-    CapabilityBoundingSet = "CAP_KILL CAP_IPC_LOCK";
-    PrivateDevices = true;
-    ProtectClock = true;
-    ProtectHostname = true;
-    ProtectKernelLogs = true;
-    ProtectKernelModules = true;
-    ProtectKernelTunables = true;
-    ProtectControlGroups = true;
-    RestrictNamespaces = true;
-    RestrictRealtime = true;
+        # rotection rules. Mostly from the `systemd-oomd` service
+        # with some of them already included upstream.
+        CapabilityBoundingSet = "CAP_KILL CAP_IPC_LOCK";
+        PrivateDevices = true;
+        ProtectClock = true;
+        ProtectHostname = true;
+        ProtectKernelLogs = true;
+        ProtectKernelModules = true;
+        ProtectKernelTunables = true;
+        ProtectControlGroups = true;
+        RestrictNamespaces = true;
+        RestrictRealtime = true;
 
-    PrivateNetwork = true;
-    IPAddressDeny = "any";
-    RestrictAddressFamilies = "AF_UNIX";
+        PrivateNetwork = true;
+        IPAddressDeny = "any";
+        RestrictAddressFamilies = "AF_UNIX";
 
-    SystemCallArchitectures = "native";
-    SystemCallFilter = ["@system-service" "~@resources @privileged"];
-  };
+        SystemCallArchitectures = "native";
+        SystemCallFilter = [
+          "@system-service"
+          "~@resources @privileged"
+        ];
+      };
       services.systembus-notify.enable = true;
 
       services.smartd = {
