2476ec1750648118515a6375869bc7c87fbd225b
me <me@o.o>
2025-12-31 05:46:28 +0100

    changes from stellar on mié 31 dic 2025 05:46:28 CET

--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -499,7 +499,7 @@ in
     })
     (mkIf cfg.earlyoom {
       services.earlyoom = {
-        # @blocksort
+        @blocksort
         killHook = '''';
         extraArgs = [ ];
         freeMemThreshold = "";
