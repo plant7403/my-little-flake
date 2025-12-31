0d94e0f8389434f1201b18c3631692f2c346ae62
me <me@o.o>
2025-12-31 08:04:39 +0100

    changes from stellar on mié 31 dic 2025 08:04:38 CET

--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -561,7 +561,7 @@ in
           [
             "-g" # kill all processes within a process group
             newPrefer # things we want to not kill
-            "--prefer '^(${appsToPrefer})$'" # things we want to kill as soon as possible
+            newPrefer # things we want to kill as soon as possible
           ];
 
         # we should ideally write the logs into a designated log file; or even better, to the journal
