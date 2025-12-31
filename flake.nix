7c11d273a81ce962a595e820f177cd40da3cb268
me <me@o.o>
2025-12-31 11:05:42 +0100

    changes from stellar on mié 31 dic 2025 11:05:42 CET

--- flake.nix
+++ flake.nix
@@ -145,9 +145,8 @@
       };
       checks = eachSystem (pkgs: {
         formatting = treefmtEval.${pkgs.system}.config.build.check self;
-        builtins.mapAttrs (_system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
-      });
-
+      })builtins.mapAttrs (_system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;;
+        checks =
 
         overlays = import ./overlays {
           inherit inputs;
