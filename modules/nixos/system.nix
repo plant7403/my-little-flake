0347b7bba7799fe4faeb986cf9bf6692b9fbcda9
me <me@o.o>
2025-12-31 08:34:04 +0100

    changes from stellar on mié 31 dic 2025 08:34:04 CET

--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -552,8 +552,7 @@ in
           [
             "-g" # kill all processes within a process group
             "--avoid '^("
-            "${appsToAvoid}"
-            "" # things we want to not kill
+            "${appsToAvoid})$'" # things we want to not kill
             newAvoid # things we want to kill as soon as possible
             "--prefer"
             "'^(${appsToPrefer})$'"
