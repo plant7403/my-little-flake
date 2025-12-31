e7432002edeca249bc87779a7ccc1d618c72491e
me <me@o.o>
2025-12-31 11:33:06 +0100

    changes from stellar on mié 31 dic 2025 11:33:06 CET

--- flake.nix
+++ flake.nix
@@ -475,8 +475,6 @@
 
       # This is highly advised, and will prevent many possible mistakes
       checks = builtins.mapAttrs (_system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
-      checks = builtins.eachSystem (pkgs: {
-        formatting = treefmtEval.${pkgs.system}.config.build.check self;
-      });
+
     };
 }
