4e310858c78768ab3966cf0136723e0f99c6c3a7
me <me@o.o>
2025-12-31 11:52:35 +0100

    changes from stellar on mié 31 dic 2025 11:52:35 CET

--- hosts/common/desktop/virtualbox.nix
+++ hosts/common/desktop/virtualbox.nix
@@ -1,21 +1,24 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   virtualisation.virtualbox.host.enable = true;
-  users.extraGroups.vboxusers.members = ["egor"];
+  users.extraGroups.vboxusers.members = [ "egor" ];
   virtualisation.libvirtd = {
     enable = true;
-    /* qemu = {
+    qemu = {
       package = pkgs.qemu_kvm;
       runAsRoot = true;
       swtpm.enable = true;
-      ovmf = {
-        enable = true;
-        packages = [
-          (pkgs.OVMF.override {
-            secureBoot = true;
-            tpmSupport = true;
-          })
-        ];
-      };
-    }; */
+      /*
+        ovmf = {
+          enable = true;
+          packages = [
+            (pkgs.OVMF.override {
+              secureBoot = true;
+              tpmSupport = true;
+            })
+          ];
+        };
+      */
+    };
   };
 }
