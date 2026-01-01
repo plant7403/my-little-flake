2c819db09da19bafb8d4549d891774ed05e9c66e
me <me@o.o>
2026-01-01 20:05:55 +0100

    changes from stellar on jue 01 ene 2026 20:05:54 CET

--- home-manager/core/theme.nix
+++ home-manager/core/theme.nix
@@ -14,13 +14,12 @@ let
   fillColor = "black";
 in
 {
+  stylix = {}
   stylix = {
     #enable = true;
     enableReleaseChecks = true;
     #stylix.image = /run/current-system/sw/share/backgrounds/gnome/vnc-d.png;
-    image = pkgs.runCommand "dimmed-background.png" { } ''
-      ${lib.getExe' pkgs.imagemagick "convert"} "${inputImage}" -brightness-contrast ${brightness},${contrast} -fill ${fillColor} $out
-    '';
+
     targets = {
       gtk = {
         flatpakSupport.enable = true;
