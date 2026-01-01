43649b3016005c9e2b0bd09201f8a64082aca7a5
me <me@o.o>
2026-01-01 19:31:45 +0100

    changes from stellar on jue 01 ene 2026 19:31:45 CET

--- flake.nix
+++ flake.nix
@@ -77,7 +77,6 @@
 
       # Small tool to iterate over each systems
       eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
-
       # Eval the treefmt modules from ./treefmt.nix
       treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
 
