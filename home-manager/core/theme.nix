ca741171b6c4045ab4429c43ab67a12ed08ad1eb
me <me@o.o>
2026-01-01 20:06:38 +0100

    changes from stellar on jue 01 ene 2026 20:06:38 CET

--- home-manager/core/theme.nix
+++ home-manager/core/theme.nix
@@ -18,7 +18,7 @@ in
     #enable = true;
     enableReleaseChecks = true;
     #stylix.image = /run/current-system/sw/share/backgrounds/gnome/vnc-d.png;
-    image = (
+    image = lib.mkForce (
       pkgs.runCommand "dimmed-background.png" { } ''
         ${lib.getExe' pkgs.imagemagick "convert"} "${inputImage}" -brightness-contrast ${brightness},${contrast} -fill ${fillColor} $out
       ''
