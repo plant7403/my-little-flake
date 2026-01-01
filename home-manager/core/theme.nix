cbe5c850e854611dcd003cd4c435cb29cb320bee
me <me@o.o>
2026-01-01 20:06:09 +0100

    changes from stellar on jue 01 ene 2026 20:06:08 CET

--- home-manager/core/theme.nix
+++ home-manager/core/theme.nix
@@ -14,7 +14,7 @@ let
   fillColor = "black";
 in
 {
-  stylix = {image = pkgs.runCommand "dimmed-background.png" { } ''
+  stylix.image = pkgs.runCommand "dimmed-background.png" { } ''
       ${lib.getExe' pkgs.imagemagick "convert"} "${inputImage}" -brightness-contrast ${brightness},${contrast} -fill ${fillColor} $out
     '';}
   stylix = {
