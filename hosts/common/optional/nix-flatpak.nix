295dfaa55adaa031b623a78778efdc3f02ec1de5
me <me@o.o>
2026-01-01 19:17:48 +0100

    changes from stellar on jue 01 ene 2026 19:17:48 CET

--- hosts/common/optional/nix-flatpak.nix
+++ hosts/common/optional/nix-flatpak.nix
@@ -1,15 +1,5 @@
 { inputs, ... }:
 {
-<<<<<<< HEAD
-  imports = [
-    inputs.nix-flatpak.nixosModules.nix-flatpak
-  ];
-
-  services.flatpak.enable = true;
-  #services.flatpak.remotes = [{ name = "flathub-beta"; location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo"; }];
-  #services.flatpak.update.onActivation = true;
-  /*
-=======
   /*
     imports = [
       inputs.nix-flatpak.nixosModules.nix-flatpak
@@ -18,23 +8,14 @@
     services.flatpak.enable = true;
     #services.flatpak.remotes = [{ name = "flathub-beta"; location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo"; }];
     services.flatpak.update.onActivation = true;
->>>>>>> 0cb1a125 (changes from stellar on mié 31 dic 2025 11:37:03 CET)
     services.flatpak.update = {
       auto = {
         enable = true;
         onCalendar = "weekly"; # Default value
       };
     };
-<<<<<<< HEAD
-  */
-  environment.persistence."/persist".directories = [
-    "/var/lib/flatpak/"
-  ];
-  xdg.portal.enable = true;
-=======
     environment.persistence."/persist".directories = [
       "/var/lib/flatpak/"
     ];
   */
->>>>>>> 0cb1a125 (changes from stellar on mié 31 dic 2025 11:37:03 CET)
 }
