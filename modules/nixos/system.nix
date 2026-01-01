503a53b16b082c89fbc7a2a947fc580cd15b59b0
me <me@o.o>
2026-01-01 23:28:03 +0100

    changes from stellar on jue 01 ene 2026 23:28:03 CET

--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -137,16 +137,6 @@ in
         #NIX_COUNT_CALLS=1
       };
       environment.sessionVariables = rec {
-        XDG_CACHE_HOME = "$HOME/.cache";
-        XDG_CONFIG_HOME = "$HOME/.config";
-        XDG_DATA_HOME = "$HOME/.local/share";
-        XDG_STATE_HOME = "$HOME/.local/state";
-
-        # Not officially in the specification
-        XDG_BIN_HOME = "$HOME/.local/bin";
-        PATH = [
-          "${XDG_BIN_HOME}"
-        ];
       };
       # NIX_CONFIG="extra-access-tokens = github.com=github_pat_XYZ" nix ...
 
