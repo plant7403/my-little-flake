879a31d0ff26d10af25495f21f46360c16ac59f2
me <me@o.o>
2026-01-01 20:06:33 +0100

    changes from stellar on jue 01 ene 2026 20:06:33 CET

--- home-manager/core/theme.nix
+++ home-manager/core/theme.nix
@@ -18,9 +18,11 @@ in
     #enable = true;
     enableReleaseChecks = true;
     #stylix.image = /run/current-system/sw/share/backgrounds/gnome/vnc-d.png;
-    image = pkgs.runCommand "dimmed-background.png" { } ''
-      ${lib.getExe' pkgs.imagemagick "convert"} "${inputImage}" -brightness-contrast ${brightness},${contrast} -fill ${fillColor} $out
-    '';
+    image = (
+      pkgs.runCommand "dimmed-background.png" { } ''
+        ${lib.getExe' pkgs.imagemagick "convert"} "${inputImage}" -brightness-contrast ${brightness},${contrast} -fill ${fillColor} $out
+      ''
+    );
     targets = {
       gtk = {
         flatpakSupport.enable = true;
