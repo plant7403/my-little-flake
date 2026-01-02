f5a7999e077a5bc92252539557be598ae0d0360c
me <me@o.o>
2026-01-02 01:08:08 +0100

    changes from stellar on vie 02 ene 2026 01:08:07 CET

--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -88,7 +88,7 @@ in
             #"cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
             #"devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+            #"cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
           ];
 
           trusted-users = [
