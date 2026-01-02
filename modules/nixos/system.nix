de1dcf68e9878f1be810e4f5b1889091f4100568
me <me@o.o>
2026-01-02 01:07:49 +0100

    changes from stellar on vie 02 ene 2026 01:07:48 CET
    changes from stellar on vie 02 ene 2026 01:07:56 CET
    
    changes from stellar on vie 02 ene 2026 01:08:07 CET
    
    changes from stellar on vie 02 ene 2026 01:13:34 CET

--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -81,7 +81,6 @@ in
           # Use the binary cache aggressively
 
           substituters = lib.mkBefore [
-            "https://aseipp-nix-cache.freetls.fastly.net"
             "https://nix-community.cachix.org"
           ];
 
@@ -89,7 +88,7 @@ in
             #"cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
             #"devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+            #"cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
           ];
 
           trusted-users = [
@@ -119,7 +118,8 @@ in
           warn-dirty = false;
           keep-going = true;
           log-lines = 20;
-          reexec = true;
+          #reexec = true;
+
         };
 
         # Garbage collection settings
